a98095e9fe9735dab1807fb6004b8b03b995e5ef
Santeri Heiskanen <heiskanens98@gmail.com>
2024-01-07 15:02:43 +0200

    Update default config for policy

--- main.py
+++ main.py
@@ -10,16 +10,15 @@ from src import training_loop
 from src.models import msa_hyper
 from src.utils import configs
 from src.utils.common import set_global_rng_seed
+
 # Ignore warning of accessing reward space directly from gymnasium.
 warnings.filterwarnings(
-    "ignore", category=UserWarning,
-    message=".*WARN: env.reward_space.*"
+    "ignore", category=UserWarning, message=".*WARN: env.reward_space.*"
 )
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def main(cfg: omegaconf.DictConfig):
-
     print(omegaconf.OmegaConf.to_yaml(cfg, resolve=True))
     cfg = configs.as_structured_config(cfg)
     configs.validate(cfg)
@@ -27,9 +26,7 @@ def main(cfg: omegaconf.DictConfig):
     set_global_rng_seed(cfg.seed)
 
     agent = msa_hyper.MSAHyper(
-        cfg.msa_hyper_cfg,
-        policy_cfg=cfg.policy_cfg,
-        critic_cfg=cfg.critic_cfg
+        cfg.msa_hyper_cfg, policy_cfg=cfg.policy_cfg, critic_cfg=cfg.critic_cfg
     )
     training_loop.train_agent(cfg, agent)
 
